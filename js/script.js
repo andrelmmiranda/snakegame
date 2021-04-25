b60d31be7d5bfa5aca5af8dab2082bb4d9f77be0
André Miranda <andrelmmiranda@gmail.com>
2021-04-25 16:38:38 -0300

    definindo fim de jogo.

--- js/script.js
+++ js/script.js
@@ -44,17 +44,24 @@ const update = (event) =>{
 
 const drawFood = () =>{
     context.fillStyle = 'red';
-    context.fillRect(food.x, food.y, box, box)
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
 
 const startGame = () =>{
-    if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0
-    if(snake[0].x < 0  && direction === 'left') snake[0].x = 16*box
+    if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
+    if(snake[0].x < 0  && direction === 'left') snake[0].x = 16 * box;
 
-    if(snake[0].y > 15 * box && direction === 'down') snake[0].y = 0
-    if(snake[0].y < 0  && direction === 'up') snake[0].y = 16*box
+    if(snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
+    if(snake[0].y < 0  && direction === 'up') snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game over');
+        }
+    }
 
     createBackground();
     createSnake();
