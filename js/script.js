d1e9cc632da3b8beed9c0f2ae07b0f98dcd4b632
André Miranda <andrelmmiranda@gmail.com>
2021-04-25 16:06:48 -0300

    definindo eventos.

--- js/script.js
+++ js/script.js
@@ -22,7 +22,30 @@ const createSnake = () =>{
     }
 }
 
+const update = (event) =>{
+    if(event.keycode == 37 && direction != 'right'){
+        direction = 'left';
+    }
+    if(event.keycode == 38 && direction !== 'down'){
+        direction = 'up';
+    }
+    if(event.keycode == 39 && direction != 'left'){
+        direction = 'right';
+    }
+    if(event.keycode == 40 && direction != 'up'){
+        direction = 'down';
+    }
+}
+
+document.addEventListener('keydown', update);
+
 const startGame = () =>{
+    if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0
+    if(snake[0].x < 0  && direction === 'left') snake[0].x = 16*box
+
+    if(snake[0].y > 15 * box && direction === 'down') snake[0].y = 0
+    if(snake[0].y < 0  && direction === 'up') snake[0].y = 16*box
+
     createBackground();
     createSnake();
 
