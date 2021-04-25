ba9913e3f726e99ab6e5a4d6a2627b92fcce692d
André Miranda <andrelmmiranda@gmail.com>
2021-04-25 16:31:13 -0300

    aumentando o tamanho da snake.

--- js/script.js
+++ js/script.js
@@ -79,7 +79,12 @@ const startGame = () =>{
         snakeY += box;
     }
 
-    snake.pop();
+    if(snakeX !== food.x || snakeY !== food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15+1) * box;
+        food.y = Math.floor(Math.random() * 15+1) * box;
+    }
 
     let newHead = {
         x: snakeX,
@@ -87,7 +92,6 @@ const startGame = () =>{
     };
 
     snake.unshift(newHead);
-
 }
 
 let game = setInterval(startGame, 100);
