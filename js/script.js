18660a4386959ba9a19d1726f06cde61b01196a2
André Miranda <andrelmmiranda@gmail.com>
2021-04-25 17:18:52 -0300

    refatorando código.

--- js/script.js
+++ js/script.js
@@ -1,65 +1,56 @@
-const canvas = document.getElementById('snake');
-const context = canvas.getContext('2d');
-const box = 32;
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
 
 let snake = [];
 snake[0] = {
-    x: 8*box,
-    y: 8*box
+    x: 8 * box,
+    y: 8 * box
 };
 
-let direction = 'right';
-
+let direction = "right";
 let food = {
-    x: Math.floor(Math.random()*15+1) * box,
-    y: Math.floor(Math.random()*15+1) * box
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
 };
 
 const createBackground = () =>{
-    context.fillStyle = 'lightgreen';
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16*box, 16*box);
-}
+};
 
 const createSnake = () =>{
-    for(let i = 0; i < snake.length; i++){
-        context.fillStyle = 'green';
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
-}
-
-const update = (event) =>{
-    if(event.keycode == 37 && direction != 'right'){
-        direction = 'left';
-    }
-    if(event.keycode == 38 && direction !== 'down'){
-        direction = 'up';
-    }
-    if(event.keycode == 39 && direction != 'left'){
-        direction = 'right';
-    }
-    if(event.keycode == 40 && direction != 'up'){
-        direction = 'down';
-    }
-}
+};
 
 const drawFood = () =>{
-    context.fillStyle = 'red';
+    context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 }
 
+const update = (event) =>{
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+};
+
 document.addEventListener('keydown', update);
 
-const startGame = () =>{
-    if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
-    if(snake[0].x < 0  && direction === 'left') snake[0].x = 16 * box;
+const startGame = () =>{    
 
-    if(snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
-    if(snake[0].y < 0  && direction === 'up') snake[0].y = 16 * box;
-
-    for(i = 1; i < snake.length; i++){
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+    
+    for(let i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert('Game over');
+            alert('Game Over :(');
         }
     }
 
@@ -70,35 +61,24 @@ const startGame = () =>{
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction === 'right'){
-        snakeX += box;
-    }
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
-    if(direction === 'left'){
-        snakeX -= box;
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); 
+    } else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-
-    if(direction === 'up'){
-        snakeY -= box;
-    }
-
-    if(direction === 'down'){
-        snakeY += box;
-    }
-
-    if(snakeX !== food.x || snakeY !== food.y){
-        snake.pop();
-    }else{
-        food.x = Math.floor(Math.random() * 15+1) * box;
-        food.y = Math.floor(Math.random() * 15+1) * box;
-    }
-
-    let newHead = {
+    
+    let newHead ={
         x: snakeX,
         y: snakeY
-    };
+    }
 
     snake.unshift(newHead);
-}
+};
 
-let game = setInterval(startGame, 100);
+let jogo = setInterval(startGame, 100);
